458c131a2f88650fff636a99f03a4d645a1f9030
sanlokaja <sandyloka790@gmail.com>
2022-12-25 16:53:23 +0700

    docs: add router collection

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import { render } from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/home/Home"
+import Home from "./pages/home/Home";
+import Collection from "./pages/collection/Collection";
 
 const root = document.getElementById('root');
 render(
     <Router>
         <Routes>
             <Route path="/" element={<Home />}></Route>
+            <Route path="/collection" element={<Collection />}></Route>
         </Routes>
     </Router>,
     root
