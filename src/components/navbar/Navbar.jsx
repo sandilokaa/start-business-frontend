56c1f49bfd24564785d44e8536c9bdf0632afcbd
sanlokaja <sandyloka790@gmail.com>
2022-12-25 16:53:49 +0700

    docs: add link collection

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,10 +6,12 @@ import {
     Form,
     Navbar
 } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "../../assets/css/style.css";
 
 const NavbarGeneral = () => {
+
+    const navigate = useNavigate();
 
     return (
         <Navbar className="navbar" expand="lg" fixed="top">
@@ -18,8 +20,8 @@ const NavbarGeneral = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link className="nav-item" >Home</Nav.Link>
-                        <Nav.Link className="nav-item" >Collection</Nav.Link>
+                        <Nav.Link className="nav-item" onClick={() => navigate(`/`)} >Home</Nav.Link>
+                        <Nav.Link className="nav-item" onClick={() => navigate(`/collection`)}>Collection</Nav.Link>
                         <Nav.Link className="nav-item" >About Us</Nav.Link>
                         <Nav.Link className="nav-item" >Contact Us</Nav.Link>
                         <Nav.Link className="nav-item" >Class</Nav.Link>
