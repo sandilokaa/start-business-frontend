85da9d9e9667b725d86fc99df3a24701b51ae055
sanlokaja <sandyloka790@gmail.com>
2023-01-11 15:31:35 +0700

    docs: remove key props

--- src/components/collection/StartBusinessCardProducts.jsx
+++ src/components/collection/StartBusinessCardProducts.jsx
@@ -3,12 +3,12 @@ import { Card, Col } from "react-bootstrap";
 import "../../assets/css/style.css";
 
 
-const StartBusinessCardProducts = ({ key, cardTitle, cardImage, cardPrice, handleClick }) => {
+const StartBusinessCardProducts = ({ cardTitle, cardImage, cardPrice, handleClick }) => {
 
     return (
 
         <Col className="col-6 col-lg-3 mt-4">
-            <Card className="sb-products" onClick={handleClick} key={key}>
+            <Card className="sb-products" onClick={handleClick}>
                 <Card.Img className="sb-products-image" src={cardImage} loading="lazy"/>
                 <Card.Body>
                     <Card.Title className="sb-products-title">{cardTitle}</Card.Title>
