46d3db879c26ff29f34510b1cab01105c6cb3df3
sanlokaja <sandyloka790@gmail.com>
2023-01-16 16:16:37 +0700

    docs: add route product detail

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Collection from "./pages/collection/Collection";
+import ProductDetail from "./pages/detail-pages/ProductDetail";
 
 const roots = document.getElementById('root');
 const root = createRoot(roots);
@@ -14,6 +15,9 @@ root.render(
         <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/collection" element={<Collection />}></Route>
+            <Route path="/collection/detail" element={<ProductDetail />}>
+                <Route path=":productId" element={<ProductDetail />}/>
+            </Route>
         </Routes>
     </Router>
 );
