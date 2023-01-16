7b04a9e269029b2255d527658d1a6693dfc3d4c9
sanlokaja <sandyloka790@gmail.com>
2023-01-16 16:17:41 +0700

    docs: add props onClick

--- src/components/collection/StartBusinessCardProducts.jsx
+++ src/components/collection/StartBusinessCardProducts.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { Card, Col } from "react-bootstrap";
 import "../../assets/css/style.css";
 
 
-const StartBusinessCardProducts = ({ cardTitle, cardImage, cardPrice, handleClick }) => {
+const StartBusinessCardProducts = ({ productId, cardTitle, cardImage, cardPrice }) => {
+
+    const navigate = useNavigate();
 
     return (
 
         <Col className="col-6 col-lg-3 mt-4">
-            <Card className="sb-products" onClick={handleClick}>
+            <Card className="sb-products" onClick={() => navigate(`/collection/detail/${productId}`)}>
                 <Card.Img className="sb-products-image" src={cardImage} loading="lazy"/>
                 <Card.Body>
                     <Card.Title className="sb-products-title">{cardTitle}</Card.Title>
