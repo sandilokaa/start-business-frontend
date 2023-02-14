7d6dede87b2bc65ee014ed7d0df5262d1450600d
sanlokaja <sandyloka790@gmail.com>
2023-02-14 15:02:09 +0700

    docs: add data tutorials

--- src/assets/data/allTutorials.js
+++ src/assets/data/allTutorials.js
@@ -1,4 +1,4 @@
-export const recomendedData = {
+export const allTutorialsData = {
     "type": "YoutubeCollection",
     "Collections": [
         {
@@ -7,7 +7,7 @@ export const recomendedData = {
             properties:
             {
                 image: "https://img.freepik.com/free-vector/online-tutorials-concept_52683-37480.jpg?w=826&t=st=1675711544~exp=1675712144~hmac=bf257ca0e72aa74c4ebfde36a971f354edc9affc6995ac1cbc1748a20eaaa40f",
-                name: "Cara Melakukan Order",
+                name: "Cara Melakukan Order Apparel",
                 description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
                 link: "https://www.youtube.com/watch?v=CK0xk4eMY9M"
             }
@@ -30,6 +30,17 @@ export const recomendedData = {
             {
                 image: "https://img.freepik.com/free-vector/online-tutorials-concept_52683-37480.jpg?w=826&t=st=1675711544~exp=1675712144~hmac=bf257ca0e72aa74c4ebfde36a971f354edc9affc6995ac1cbc1748a20eaaa40f",
                 name: "Cara Membuat Mockup di PicArt",
+                description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                link: "https://www.youtube.com/watch?v=CK0xk4eMY9M"
+            }
+        },
+        {
+            type: "Youtube",
+            id: "4",
+            properties:
+            {
+                image: "https://img.freepik.com/free-vector/online-tutorials-concept_52683-37480.jpg?w=826&t=st=1675711544~exp=1675712144~hmac=bf257ca0e72aa74c4ebfde36a971f354edc9affc6995ac1cbc1748a20eaaa40f",
+                name: "Perbedaan Kaos 30s dengan 20s",
                 description: "Some quick example text to build on the card title and make up the bulk of the card's content.",
                 link: "https://www.youtube.com/watch?v=CK0xk4eMY9M"
             }
